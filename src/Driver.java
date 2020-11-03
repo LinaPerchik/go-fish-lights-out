46714a8021b533674e23bed4d86941665676d4ef
ibragp05 <ibragp05@uwosh.edu>
2020-11-03 13:54:34 -0600

    Uncommented lights out game in the driver class

--- src/Driver.java
+++ src/Driver.java
@@ -19,7 +19,6 @@ public class Driver {
         playerName1 = scanner.next();
         System.out.println("Please enter the name of player #2: ");
         playerName2 = scanner.next();
-        /*
         System.out.println("Please enter the size of the board: ");
         boardSize = scanner.nextInt();
         // Creating objects representing the 2 players
@@ -29,10 +28,10 @@ public class Driver {
         LightsOut lightsOut = new LightsOut(player1, player2, boardSize);
         // Starting the game
         lightsOut.play();
-         */
+        // Creating objects representing the 2 players
         GoFishPlayer player3 = new GoFishPlayer(playerName1);
         GoFishPlayer player4 = new GoFishPlayer(playerName2);
-        // Creating an object representing the game of lights out
+        // Creating an object representing the game of go fish
         GoFish goFish = new GoFish(player3, player4);
         // Starting the game
         goFish.play();
